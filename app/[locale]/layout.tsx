5e5d8a6de0dffd8810d2102dfffe5914b732582a
luca-schr <lucas.schrever@outlook.com>
2026-09-02 18:10:40 +0200

    feat: start design system -> colors

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,14 +1,17 @@
-import {NextIntlClientProvider} from 'next-intl';
- 
+import './../globals.css';
+import { NextIntlClientProvider } from 'next-intl';
+
 type Props = {
   children: React.ReactNode;
 };
- 
-export default async function RootLayout({children}: Props) {
+
+export default async function RootLayout({ children }: Props) {
   return (
     <html>
-      <body>
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      <body className="bg-background text-foreground">
+        <NextIntlClientProvider>
+          {children}
+        </NextIntlClientProvider>
       </body>
     </html>
   );
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,5 +2,9 @@ import {useTranslations} from 'next-intl';
 
 export default function HomePage() {
   const t = useTranslations('HomePage');
-  return <h1>{t('title')}</h1>;
+  return (
+    <>
+      <h1>{t('title')}</h1>
+    </>
+  )
 }
